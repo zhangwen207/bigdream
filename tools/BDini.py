9cecde9d9c18670d8534992a1508f385b555a90b
zhangwen207 <201641869@qq.com>
2016-05-26 22:40:45 +0800

    测试

--- tools/BDini.py
+++ tools/BDini.py
@@ -5,8 +5,8 @@
 全局变量
 """
 
-DataBase='mysql+pymysql://me:me@10.8.7.31/bigdream?charset=utf8'
-#DataBase='mysql+pymysql://me:me@192.168.83.129/bigdream?charset=utf8'
+#DataBase='mysql+pymysql://me:me@10.8.7.31/bigdream?charset=utf8'
+DataBase='mysql+pymysql://me:me@192.168.83.129/bigdream?charset=utf8'
 #SCXmail='zhangwen@efunds.com.cn'
 from_addr = '201641869@qq.com'
 user_pass ='fnzpwvosvezpcbdh'   #申请 http://service.mail.qq.com/cgi-bin/help?subtype=1&&id=28&&no=1001256'
