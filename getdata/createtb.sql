13b64f7a20b464d71d93bcf73fb07df8e64ff3a6
zhangwen207 <201641869@qq.com>
2016-06-04 13:52:15 +0800

    version 1.0

--- getdata/createtb.sql
+++ getdata/createtb.sql
@@ -8,10 +8,13 @@ TStamp datetime NOT NULL
 drop table score; 
 CREATE TABLE score 
 (
+date datetime NOT NULL,
+code varchar(10) NOT NULL,
 clname varchar(50) NOT NULL,
 buy int,
 sell int,
 profit float(53),
 ratio float(53),
-date datetime NOT NULL
+buymark int,
+sellmark int
 );
